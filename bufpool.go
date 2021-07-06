474320594d30fa251f7584bf51af2b39a522f9bc
Andrei Pelinescu-Onciul <andrei@iptel.org>
2021-07-06 10:24:10 +0200

    fix slog buffer pool race
    Switch from atomic lock-less to a simpler one bpool lock.
    Fixes race, uses less atomic operations but more "contended".
    
    Fixes #2.

--- bufpool.go
+++ bufpool.go
@@ -8,29 +8,26 @@ package slog
 
 import (
 	"bytes"
-	"runtime"
-	"sync/atomic"
-	"unsafe"
+	"fmt"
+	"sync"
 )
 
 // keep a pool of bytes.Buffer(s) for fast re-use
 const (
-	BufPoolSz = 128 // maximum buffer in pool, must be 2^k
-	BufSz     = 1024
-	MaxBufSz  = 65534
-
-//	BufSz     = 1024  // typical buf sz
-//	MaxBufSz  = 65534 // max allowed (if more reset)
+	BufPoolSz = 128   // maximum buffer in pool, must be 2^k
+	BufSz     = 1024  // typical buf sz
+	MaxBufSz  = 65534 // max allowed (if more reset)
 )
 
 type bpool struct {
+	lock sync.Mutex
+	// always push at head%BufPoolSz
+	head uint64 // last free (always increases)
+	// always pop from tail%BufPoolSz
+	tail uint64 // first free (idx is module BufPoolSZ) (always increases)
 	// BufPoolSz must be 2^k to avoid wraparounds problem with first & last
 	// and module BufPoolSz.
 	pool [BufPoolSz]*bytes.Buffer
-	// always pop from tail%BufPoolSz
-	tail uint64 // first free (idx is module BufPoolSZ) (always increases)
-	// always push at head%BufPoolSz
-	head uint64 // last free (always increases)
 }
 
 func (p *bpool) idx(h uint64) uint64 {
@@ -41,54 +38,62 @@ func (p *bpool) push(sb *bytes.Buffer) bool {
 	if sb == nil {
 		return false
 	}
-	var h uint64
-	for {
-		t := atomic.LoadUint64(&p.tail)
-		h = atomic.LoadUint64(&p.head)
-		if (h - t) >= BufPoolSz {
-			return false // full
-		}
-		// increase
-		if atomic.CompareAndSwapUint64(&p.head, h, h+1) {
-			// got  a safe slot for storing
-			break
-		}
+	p.lock.Lock()
+	t := p.tail
+	h := p.head
+	if (h - t) >= BufPoolSz {
+		p.lock.Unlock()
+		return false // full
 	}
+	// increase
+	p.head++
 	i := p.idx(h)
-	atomic.StorePointer((*unsafe.Pointer)(unsafe.Pointer(&p.pool[i])),
-		unsafe.Pointer(sb))
+	// sanity checks
+	if p.pool[i] != nil {
+		errStr := fmt.Sprintf("push(): unexpected non-null pointer at h: %d"+
+			" (crt head %d), tail %d (crt %d) i %d : %p\n",
+			h, p.head, t, p.tail, i, p.pool[i])
+		p.lock.Unlock()
+		panic(errStr)
+		return false
+	}
+	p.pool[i] = sb
+	p.lock.Unlock()
 	return true
 }
 
 func (p *bpool) pop() *bytes.Buffer {
-	var ret unsafe.Pointer
-	var n uint
-	for {
-		h := atomic.LoadUint64(&p.head)
-		t := atomic.LoadUint64(&p.tail)
-		if h == t {
-			// empty
-			return nil
-		}
-		// try to take ownership of the pointer
-		i := p.idx(t)
-		ret = atomic.SwapPointer((*unsafe.Pointer)(unsafe.Pointer(&p.pool[i])),
-			nil)
-		if ret != nil {
-			atomic.AddUint64(&p.tail, 1)
-			break
-		}
-		// else some other pop() won this race => retry
-		n++
-		runtime.Gosched()
+	p.lock.Lock()
+	h := p.head
+	t := p.tail
+	if h == t {
+		// empty
+		p.lock.Unlock()
+		return nil
 	}
-	return (*bytes.Buffer)(ret)
+	// "reserve" a tail index
+	p.tail++
+	i := p.idx(t)
+	ret := p.pool[i]
+	// sanity checks
+	if ret == nil {
+		errStr := fmt.Sprintf("pop(): unexpected null pointer at h: %d"+
+			" (crt head %d), tail %d (crt %d) i %d : %p\n",
+			h, p.head, t, p.tail, i, p.pool[i])
+		p.lock.Unlock()
+		panic(errStr)
+		return nil
+	}
+	p.pool[i] = nil
+	p.lock.Unlock()
+	return ret
 }
 
 func (p *bpool) free() uint64 {
-	h := atomic.LoadUint64(&p.head)
-	t := atomic.LoadUint64(&p.tail)
-	return h - t
+	p.lock.Lock()
+	ret := p.head - p.tail
+	p.lock.Unlock()
+	return ret
 }
 
 func (p *bpool) init() bool {
